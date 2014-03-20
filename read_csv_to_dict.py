34d609cc884aeefb5fa79e3d2f14b0b2c7c8c17a
Sebastian Raschka <se.raschka@me.com>
2014-03-19 20:48:23 -0400

    Update read_csv_to_dict.py

--- read_csv_to_dict.py
+++ read_csv_to_dict.py
@@ -1,9 +1,13 @@
 # Sebastian Raschka, 03/2014
-# Read a csv file (with a header) into a dictionary
-# where the dictionary structure will be
-#   {header_col1: [val1_line1, val1_line2, ...], header_col2: [val2_line1, val2_line2, ...], ...}
+
 
 def read_csv(csv_path):
+    """
+    Read a csv file (with a header) into a dictionary
+    where the dictionary structure will be
+    {header_col1: [val1_line1, val1_line2, ...], header_col2: [val2_line1, val2_line2, ...], ...}
+    
+    """
     with open(csv_path, 'r') as in_csv:
         header = in_csv.readline().strip().split(',')
         data = {i:[] for i in header}
