8c3461624f22b84063285f79949a3135a0f04766
Sebastian Raschka <se.raschka@me.com>
2014-04-01 15:59:49 -0400

    Update conc_gzip_files.py

--- useful_scripts/conc_gzip_files.py
+++ useful_scripts/conc_gzip_files.py
@@ -14,15 +14,15 @@ def conc_gzip_files(in_dir, out_file, append=False):
              else creates a new output file.
 
     """
-    write_mode = 'w'
+    write_mode = 'bw'
     if append:
-        write_mode = 'a'
+        write_mode = 'ba'
     gzips = [os.path.join(in_dir, i) for i in os.listdir(in_dir) if i.endswith('.gz')]
     with open(out_file, write_mode) as ofile:
         for f in gzips:
             with gzip.open(f, 'rb') as gzipf:
                 for line in gzipf:
-                    ofile.write(line.decode())
+                    ofile.write(line)
 
 if __name__ == '__main__':
     conc_gzip_files('/home/usr/my_dir', '/home/usr/test.txt')
