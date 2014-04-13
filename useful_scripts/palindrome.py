47e4aca99ceba0d469efc78fffdc073c2f2a962c
rasbt <se.raschka@me.com>
2014-04-13 15:52:40 -0400

    palindrome

---
+++ useful_scripts/palindrome.py
@@ -0,0 +1,14 @@
+# Sebastian Raschka 04/2014
+
+def palindrome(my_str):
+    """
+    Returns True if an input string is a palindrom
+    """
+    stripped_str = "".join([l.lower() for l in my_str if l.isalpha()])
+    return stripped_str == stripped_str[::-1]
+
+if __name__ == '__main__':
+    test1 = 'Hello World!'
+    test2 = "Go hang a salami. I'm a lasagna hog."
+    print('test1', palindrome(test1))
+    print('test2',  palindrome(test2))
