ad32c82ee9db5179ce910bb169e495a5dfb9ab91
rasbt <se.raschka@me.com>
2014-05-05 11:40:43 -0400

    removed temp cython files

--- benchmarks/setup.py
+++
@@ -1,9 +0,0 @@
-
-from distutils.core import setup
-from distutils.extension import Extension
-from Cython.Distutils import build_ext
-
-setup(
-    cmdclass = {'build_ext': build_ext},
-    ext_modules = [Extension("ccy_classic_lstsqr", ["ccy_classic_lstsqr.pyx"])]
-)
