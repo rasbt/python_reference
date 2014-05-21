8ca2852356c9b9725432e719669fccd1e33761e9
rasbt <se.raschka@me.com>
2014-05-21 19:20:06 -0400

    syntax fix

--- useful_scripts/prepend_python_shebang.sh
+++ useful_scripts/prepend_python_shebang.sh
@@ -1,16 +1,16 @@
 # Sebastian Raschka 05/21/2014
 # Shell script that prepends a Python shebang 
-# e.g., `!#/usr/bin/python` to all
+# '#!/usr/bin/env python' to all
 # Python script files in the current directory
 # so that script files can be executed via
 # >> myscript.py 
 # instead of 
 # >> python myscript.py
 
-# prepends e.g., !#/usr/bin/python to all .py files
+# prepends '#!/usr/bin/env python' to all .py files
 
 find ./ -maxdepth 1 -name "*.py" -exec sed -i.bak '1i\
-#!'"$(which python)"'
+#!/usr/bin/env python
 ' {} \;
 
 # removes temporary files
