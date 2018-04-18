5da40c30c01aeb5a45aeffcbe3446dd51ce42116
rasbt <mail@sebastianraschka.com>
2018-04-18 09:47:16 -0400

    fix cursor use

--- tutorials/sqlite3_howto/code/print_db_info.py
+++ tutorials/sqlite3_howto/code/print_db_info.py
@@ -22,52 +22,57 @@ time: 1
 
 import sqlite3
 
+
 def connect(sqlite_file):
     """ Make connection to an SQLite database file """
     conn = sqlite3.connect(sqlite_file)
     c = conn.cursor()
     return conn, c
 
+
 def close(conn):
     """ Commit changes and close connection to the database """
-    #conn.commit()
+    # conn.commit()
     conn.close()
+
 
 def total_rows(cursor, table_name, print_out=False):
     """ Returns the total number of rows in the database """
-    c.execute('SELECT COUNT(*) FROM {}'.format(table_name))
-    count = c.fetchall()
+    cursor.execute('SELECT COUNT(*) FROM {}'.format(table_name))
+    count = cursor.fetchall()
     if print_out:
         print('\nTotal rows: {}'.format(count[0][0]))
     return count[0][0]
 
+
 def table_col_info(cursor, table_name, print_out=False):
-    """ 
-       Returns a list of tuples with column informations:
-      (id, name, type, notnull, default_value, primary_key)
-    
+    """ Returns a list of tuples with column informations:
+    (id, name, type, notnull, default_value, primary_key)
     """
-    c.execute('PRAGMA TABLE_INFO({})'.format(table_name))
-    info = c.fetchall()
-    
+    cursor.execute('PRAGMA TABLE_INFO({})'.format(table_name))
+    info = cursor.fetchall()
+
     if print_out:
         print("\nColumn Info:\nID, Name, Type, NotNull, DefaultVal, PrimaryKey")
         for col in info:
             print(col)
     return info
 
+
 def values_in_col(cursor, table_name, print_out=True):
-    """ Returns a dictionary with columns as keys and the number of not-null 
-        entries as associated values.
+    """ Returns a dictionary with columns as keys
+    and the number of not-null entries as associated values.
     """
-    c.execute('PRAGMA TABLE_INFO({})'.format(table_name))
-    info = c.fetchall()
+    cursor.execute('PRAGMA TABLE_INFO({})'.format(table_name))
+    info = cursor.fetchall()
     col_dict = dict()
     for col in info:
         col_dict[col[1]] = 0
     for col in col_dict:
-        c.execute('SELECT ({0}) FROM {1} WHERE {0} IS NOT NULL'.format(col, table_name))
-        # In my case this approach resulted in a better performance than using COUNT
+        c.execute('SELECT ({0}) FROM {1} '
+                  'WHERE {0} IS NOT NULL'.format(col, table_name))
+        # In my case this approach resulted in a
+        # better performance than using COUNT
         number_rows = len(c.fetchall())
         col_dict[col] = number_rows
     if print_out:
@@ -85,7 +90,7 @@ if __name__ == '__main__':
     conn, c = connect(sqlite_file)
     total_rows(c, table_name, print_out=True)
     table_col_info(c, table_name, print_out=True)
-    values_in_col(c, table_name, print_out=True) # slow on large data bases
-    
-    close(conn)
+    # next line might be slow on large databases
+    values_in_col(c, table_name, print_out=True)
 
+    close(conn)
